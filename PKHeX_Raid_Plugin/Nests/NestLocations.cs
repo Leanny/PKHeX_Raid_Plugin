333ced74c01ebead1a37d957de10ce4f622eb46e
Kurt <kaphotics@gmail.com>
2019-12-20 17:18:44 -0800

    Manually merge the other commits from an hour ago

--- PKHeX_Raid_Plugin/Nests/NestLocations.cs
+++ PKHeX_Raid_Plugin/Nests/NestLocations.cs
@@ -33,9 +33,9 @@
             new NestHashDetail(0x17459356dc635afd, 0xba8746671015cd43, 12, 209, 976), //39 65
             new NestHashDetail(0x17428356dc6109f6, 0xba805967100fcede, 12, 136, 906), //01 48
             new NestHashDetail(0x17458b56dc634d65, 0x450421d99cf882c1, 12, 252, 905), //31 90
-            new NestHashDetail(0x17491656dc666f6d, 0xba83da671012dfe8, 02, 011, 851), //26 59
+            new NestHashDetail(0x17491656dc666f6d, 0xba83da671012dfe8, 02, 030, 927), //26 59
             new NestHashDetail(0x17490856dc6657a3, 0x17491556dc666dba, 02, 012, 851), //28 79
-            new NestHashDetail(0x17491656dc666f6d, 0x17491556dc666dba, 02, 012, 861), //26 58
+            new NestHashDetail(0x17491656dc666f6d, 0xba83db671012e19b, 02, 012, 861), //26 58
             new NestHashDetail(0x17428856dc611275, 0x45041fd99cf87f5b, 02, 020, 913), //04 92
             new NestHashDetail(0x17491656dc666f6d, 0xba83da671012dfe8, 02, 040, 878), //26 59
             new NestHashDetail(0x17428256dc610843, 0xba805367100fc4ac, 15, 052, 776), //02 50
@@ -44,8 +44,8 @@
             new NestHashDetail(0x17459356dc635afd, 0xba8746671015cd43, 04, 158, 705), //39 65
             new NestHashDetail(0x17458b56dc634d65, 0xba83d8671012dc82, 04, 220, 759), //31 61
             new NestHashDetail(0x17458b56dc634d65, 0xba83d8671012dc82, 04, 248, 852), //31 61
-            new NestHashDetail(0x17428b56dc61178e, 0x17428556dc610d5c, 16, 129, 818), //09 75
-            new NestHashDetail(0x17428b56dc61178e, 0x17428556dc610d5c, 16, 131, 735), //09 75
+            new NestHashDetail(0x17428b56dc61178e, 0xba8a4e67101810b2, 16, 129, 818), //09 75
+            new NestHashDetail(0x17428b56dc61178e, 0xba8a4e67101810b2, 16, 131, 735), //09 75
             new NestHashDetail(0x17501a56dc6c94e7, 0xba805d67100fd5aa, 16, 105, 907), //44 42
             new NestHashDetail(0x17428556dc610d5c, 0xba805d67100fd5aa, 16, 050, 909), //07 44
             new NestHashDetail(0x17428b56dc61178e, 0x450420d99cf8810e, 16, 098, 750), //09 91
@@ -70,7 +70,7 @@
             new NestHashDetail(0x173efe56dc5df220, 0xba805967100fcede, 11, 443, 792), //10 48
             new NestHashDetail(0x173f0256dc5df8ec, 0xba805367100fc4ac, 10, 412, 533), //14 50
             new NestHashDetail(0x17458a56dc634bb2, 0xba83d9671012de35, 10, 345, 537), //30 60
-            new NestHashDetail(0x17491456dc666c07, 0x17491356dc666a54, 10, 365, 559), //24 56
+            new NestHashDetail(0x17491456dc666c07, 0xba83dd671012e501, 10, 365, 559), //24 56
             new NestHashDetail(0x17501c56dc6c984d, 0xba8746671015cd43, 10, 408, 570), //40 65
             new NestHashDetail(0x17458656dc6344e6, 0x45009dd99cf56c9e, 01, 193, 295), //34 82
             new NestHashDetail(0x173f0156dc5df739, 0x450424d99cf887da, 01, 274, 321), //13 87
@@ -91,16 +91,16 @@
             new NestHashDetail(0x173f0056dc5df586, 0x450098d99cf5641f, 14, 303, 242), //12 85
             new NestHashDetail(0x17491256dc6668a1, 0xba83de671012e6b4, 14, 324, 209), //22 55
             new NestHashDetail(0x173f0256dc5df8ec, 0xba805367100fc4ac, 14, 326, 219), //14 50
-            new NestHashDetail(0x173f0656dc5dffb8, 0x173f0756dc5e016b, 14, 330, 215), //18 54
+            new NestHashDetail(0x173f0656dc5dffb8, 0xba83df671012e867, 14, 330, 215), //18 54
             new NestHashDetail(0x17458756dc634699, 0x17458456dc634180, 14, 282, 195), //35 63
             new NestHashDetail(0x173f0556dc5dfe05, 0x45041ed99cf87da8, 03, 265, 139), //17 93
             new NestHashDetail(0x173f0556dc5dfe05, 0xba83e1671012ebcd, 03, 307, 093), //17 52
             new NestHashDetail(0x173f0356dc5dfa9f, 0x45041ed99cf87da8, 03, 331, 084), //15 93
-            new NestHashDetail(0x17428b56dc61178e, 0x17428556dc610d5c, 03, 219, 099), //09 75
+            new NestHashDetail(0x17428b56dc61178e, 0xba8a4e67101810b2, 03, 219, 099), //09 75
             new NestHashDetail(0x173eff56dc5df3d3, 0xba805a67100fd091, 03, 243, 120), //11 49
             new NestHashDetail(0x173efe56dc5df220, 0xba805967100fcede, 03, 262, 174), //10 48
             new NestHashDetail(0x17490f56dc666388, 0xba83de671012e6b4, 03, 283, 098), //21 55
-            new NestHashDetail(0x173f0656dc5dffb8, 0x173f0756dc5e016b, 03, 304, 112), //20 54
+            new NestHashDetail(0x173f0656dc5dffb8, 0xba83df671012e867, 03, 304, 112), //20 54
             new NestHashDetail(0x17490856dc6657a3, 0xba805c67100fd3f7, 06, 306, 145), //28 43
             new NestHashDetail(0xba8748671015d0a9, 0x450423d99cf88627, 03, 214, 168), //36 88
             new NestHashDetail(0x17501a56dc6c94e7, 0xba874c671015d775, 06, 334, 145), //42 67
@@ -119,8 +119,8 @@
             new NestHashDetail(0x173f0656dc5dffb8, 0xba8750671015de41, 05, 193, 173), //18 71
             new NestHashDetail(0x17458856dc63484c, 0x450422d99cf88474, 05, 202, 095), //32 89
             new NestHashDetail(0x17491456dc666c07, 0x4500a1d99cf5736a, 05, 185, 135), //24 78
-            new NestHashDetail(0x17491356dc666a54, 0x17491356dc666a54, 09, 170, 035), //25 56
-            new NestHashDetail(0x173f0656dc5dffb8, 0xba83e0671012ea1a, 09, 128, 058), //18 77
+            new NestHashDetail(0x17491356dc666a54, 0xba83dd671012e501, 09, 170, 035), //25 56
+            new NestHashDetail(0x173f0656dc5dffb8, 0x4500a0d99cf571b7, 09, 128, 058), //18 77
             new NestHashDetail(0x17428c56dc611941, 0xba805d67100fd5aa, 09, 161, 080), //08 44
             new NestHashDetail(0x17458656dc6344e6, 0xba8a4f6710181265, 09, 143, 039), //34 74
         };
