78015208ecec5b311f8e7a1d9e2de0df0bcc48fb
Kurt <kaphotics@gmail.com>
2019-12-20 21:37:23 -0800

    Fix inverted condition
    oops

--- PKHeX_Raid_Plugin/Util/Util.cs
+++ PKHeX_Raid_Plugin/Util/Util.cs
@@ -3,8 +3,8 @@
     public static class Util
     {
         public static bool IsHex(char c) => (c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f');
-        public static bool IsDigitOrControl(char c) => !char.IsDigit(c) && !char.IsControl(c);
-        public static bool IsHexOrControl(char c) => !IsHex(c) && !char.IsControl(c);
+        public static bool IsDigitOrControl(char c) => char.IsDigit(c) || char.IsControl(c);
+        public static bool IsHexOrControl(char c) => IsHex(c) || char.IsControl(c);
 
         public static int NumberOfSetBits(int i)
         {
